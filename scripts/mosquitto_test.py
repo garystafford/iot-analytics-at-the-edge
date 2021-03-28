44d445ffd9e492b230bfdf64c9572f8133b0f4b2
Gary A. Stafford <garystafford@rochester.rr.com>
2021-03-28 11:12:32 -0400

    Update versions

--- scripts/mosquitto_test.py
+++ scripts/mosquitto_test.py
@@ -1,50 +1,51 @@
 import argparse
 import logging
 import sys
-from datetime import datetime
 
 import paho.mqtt.publish as publish
 
 # Test Message to Mosquitto Script
 # Author: Gary A. Stafford
 # Date: 2021-03-26
-# Usage: python3 sensor_data_to_mosquitto.py \
-#           --host "192.168.1.12" --port 1883 \
-#           --topic "sensor/output"
+# Usage: python3 ./mosquitto_test.py --host "192.168.1.12" --port 1883
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
 def main():
-    args = parse_args()
-    publish_message_to_db(args)
-
-
-def date_converter(o):
-    if isinstance(o, datetime):
-        return o.__str__()
+    publish_message_to_db(parse_args())
 
 
 def publish_message_to_db(args):
-    sample_message_json = {"data": {"co": 0.004997145778992383, "humidity": 52.599998474121094, "light": False,
-                                    "lpg": 0.007696788375166227, "motion": False, "smoke": 0.020542288909950537,
-                                    "temperature": 20.600000381469727},
-                           "device_id": "b8:27:eb:bf:9d:51", "time": "2021-03-27 00:49:05.081242+00:00"}
+    """Publish test message"""
+
+    sample_message_json = {
+        "data": {
+            "co": 0.009,
+            "humidity": 59.9,
+            "light": False,
+            "lpg": 0.009,
+            "motion": False,
+            "smoke": 0.009,
+            "temperature": 19.9
+        },
+        "device_id": "00:00:00:00:00:00",
+        "time": "2021-03-27 00:00:00.000000+00:00"
+    }
     logger.debug(sample_message_json)
-    logger.debug(args)
-    publish.single(args.topic, payload=sample_message_json, hostname=args.host, port=args.port)
+    publish.single('sensor/test', payload=str(sample_message_json), hostname=args.host, port=args.port)
 
 
-# Read in command-line parameters
 def parse_args():
+    """Read in command-line parameters"""
+
     parser = argparse.ArgumentParser(description='Script arguments')
     parser.add_argument('--host', help='Mosquitto host', default='localhost')
     parser.add_argument('--port', help='Mosquitto port', type=int, default=1883)
-    parser.add_argument('--topic', help='Mosquitto topic', default='paho/test')
 
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
